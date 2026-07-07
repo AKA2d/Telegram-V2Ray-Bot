84e535a5e91d395c9957e595bc75ed8d00e48eed
AKA2d <aka2ds00@gmail.com>
2026-07-07 16:06:32 +0330

    updated texts

--- telegram-bot/bot/handlers/admin/base.py
+++ telegram-bot/bot/handlers/admin/base.py
@@ -9,6 +9,6 @@ class AdminOnlyMiddleware(BaseMiddleware):
         user = data.get("event_from_user")
         if user is None or not is_admin(user.id):
             if isinstance(event, Message):
-                await event.answer("شما به این بخش دسترسی ندارید.")
+                await event.answer( "شما به این بخش دسترسی ندارید. \n برای بازگشت به منوی اصلی. \n /start")
             return
         return await handler(event, data)
